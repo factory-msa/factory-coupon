42becb79a074d4d9ede56758a2ac749c8e8534d1
juhyun <zzang9ha@naver.com>
2023-10-17 23:09:04 +0900

    feat: add Coupon entity

---
+++ src/main/kotlin/com/factorycoupon/domain/entity/CouponEntity.kt
@@ -0,0 +1,38 @@
+package com.factorycoupon.domain.entity
+
+import support.domain.BaseTimeEntity
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.Id
+
+@Entity
+class CouponEntity(
+
+    @Id
+    @Column(name = "COUPON_NUMBER", nullable = false)
+    var couponNumber: String,
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "COUPON_STATUS_CODE", nullable = false)
+    var couponStatusCode: CouponStatusCode,
+
+    @Column(name = "EXPIRED_FROM", nullable = false)
+    var expiredFrom: LocalDateTime,
+
+    @Column(name = "EXPIRED_TO", nullable = false)
+    var expiredTo: LocalDateTime,
+
+    @Column(name = "ISSUANCE_ID", nullable = false)
+    var issuanceId: String,
+
+    @Column(name = "PRICE", nullable = false)
+    var price: Long
+): BaseTimeEntity() {
+
+    @Column(name = "DELETED", nullable = false)
+    private var deleted: Boolean = false
+
+}
---
+++ src/main/kotlin/com/factorycoupon/domain/entity/CouponStatusCode.kt
@@ -0,0 +1,11 @@
+package com.factorycoupon.domain.entity
+
+enum class CouponStatusCode(val codeName: String) {
+
+    CPN001("미사용"),
+
+    CPN002("사용완료"),
+
+    CPN003("폐기")
+
+}
