a85303181dcab363211eeca6d76a5115484ff454
juhyun <zzang9ha@naver.com>
2023-10-29 00:05:30 +0900

    chore: add Kotlin allOpen, noArg config

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,16 @@ plugins {
     kotlin("plugin.jpa") version "1.6.21"
 }
 
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+noArg {
+    annotation("javax.persistence.Entity")
+}
+
 group = "com"
 version = "0.0.1-SNAPSHOT"
 
@@ -22,7 +32,7 @@ repositories {
 extra["springCloudVersion"] = "2021.0.8"
 
 dependencies {
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
