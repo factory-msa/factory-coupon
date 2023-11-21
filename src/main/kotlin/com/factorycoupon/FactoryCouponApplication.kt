677f62b24d5d4e189d4e38bb2f89e1324c803c35
juhyun <zzang9ha@naver.com>
2023-11-22 00:31:14 +0900

    refactor: change cloud.neflix EurekaClient -> cloud.client DiscoveryClient

--- src/main/kotlin/com/factorycoupon/FactoryCouponApplication.kt
+++ src/main/kotlin/com/factorycoupon/FactoryCouponApplication.kt
@@ -3,9 +3,8 @@ package com.factorycoupon
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
-@EnableEurekaClient
+@EnableDiscoveryClient
 @SpringBootApplication
 class FactoryCouponApplication
 
