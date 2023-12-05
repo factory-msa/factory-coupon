ff005aa8b9af5921b467ff625f350af92da361dc
juhyun <zzang9ha@naver.com>
2023-12-05 23:35:00 +0900

    refactor: change api uri (for gateway service)

--- src/main/kotlin/com/factorycoupon/api/v1/CouponGeneratorController.kt
+++ src/main/kotlin/com/factorycoupon/api/v1/CouponGeneratorController.kt
@@ -11,7 +11,7 @@ class CouponGeneratorController(
     private val service: CouponGeneratorService
 ) {
 
-    @PostMapping("/api/v1/issue-coupons")
+    @PostMapping("/api/v1/coupon/coupons/issue")
     fun generateCoupons(
         @RequestBody command: CouponGeneratorService.CouponIssuanceApiRequest
     ): ApiResponse<CouponGeneratorService.CouponIssuanceApiResponse> {
