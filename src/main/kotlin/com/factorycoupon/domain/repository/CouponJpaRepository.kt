9de537ad0c2fca14d049733af47a6dd95eea1c96
juhyun <zzang9ha@naver.com>
2024-01-27 22:36:24 +0900

    refactor: remove bracket

--- src/main/kotlin/com/factorycoupon/domain/repository/CouponJpaRepository.kt
+++ src/main/kotlin/com/factorycoupon/domain/repository/CouponJpaRepository.kt
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface CouponJpaRepository: JpaRepository<CouponEntity, String> {
-}
+interface CouponJpaRepository : JpaRepository<CouponEntity, String>
